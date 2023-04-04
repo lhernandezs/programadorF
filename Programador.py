697d3e4141779acab2d2ac6db02fedf4d115314c
lhernandezs <lhernandezs331@gmail.com>
2023-04-04 18:02:17 -0500

    commit 4 de abril

--- Programador.py
+++ Programador.py
@@ -1,80 +1,110 @@
 from Evento import Evento
-from DiasLaborables import DiasLaborables
-from dateutil.relativedelta import relativedelta
-import datetime
+from Mes import Mes
+from datetime import datetime, timedelta, date
 
 class Programador:
+    # constructor de la clase
     def __init__(self, listaEventos, mes, horasAProgramar):
-        self.__listaEventos = listaEventos
-        self.__mes = mes
-        self.__horasAProgramar = horasAProgramar
+        self._listaEventos = listaEventos
+        self._mes = mes
+        self._horasAProgramar = horasAProgramar
 
-    # este metodo retorna True si el evento está en el Dia y la Hora pasado como parametros
+    # retorna True si el evento está en el Dia y la Hora pasado como parametros o False en caso contrario
+    def eventoEnDiaHora(self, evento, x, y):
+        fecha = date(2023, self._mes, x)
+        return True if evento.fechaI <= fecha and fecha <= evento.fechaF and evento.horaI <= y and y < evento.horaF else False
 
-    def eventoEnDiaHora(self, evento, i, j):
-#      print(evento.getFechaI(), " ", evento.getFechaF(), " ", i, " ", j)
-        dia = i
-        if (datetime.date(2023,self.__mes,1) + relativedelta(day=31)).day >= dia:
-#            print("Entro", datetime.date(2023,self.__mes,1) + relativedelta(31))
-            if evento.getFechaI() <= datetime.date(2023, self.__mes, dia) and datetime.date(2023, self.__mes, dia) <= evento.getFechaF() \
-                and evento.getHoraI() <= j and j < evento.getHoraF(): 
-#                print(" Adicionar .....", evento.getFechaI(), " ", evento.getFechaF(), " ", i, " ", j )
-                return True
-        return False
-            
-    # este metodo retorna una lista de igual longitud a la lista de eventos, con tuplas (horaI, horaF, diaI, diaF) de los eventos depurando que los cruces. Los cruces se resolveran tratando de colocar horas "similares" a todas
-    def cruceDeEventos(self):
-        programacionBruta = [[[] for j in range(24)] for i in range(1,32)]
-        for e in range(len(self.__listaEventos)):
-            for i in range(1,32):
+    # retorna una Matriz de los eventos en cada dia y la hora del mes    
+    def eventosPorDiaHora(self):
+        diasDelMes = Mes(self._mes).ultimoDia().day
+        eventosPorDiaHora = [[[] for j in range(24)] for i in range(diasDelMes)]
+        for e in range(len(self._listaEventos)):
+            for i in range(diasDelMes):
                 for j in range(24):
-                    if self.eventoEnDiaHora(self.__listaEventos[e], i, j) and i in DiasLaborables(self.__mes).diasLaborablesMes():
-                        programacionBruta[i][j].append(e)
+                    if self.eventoEnDiaHora(self._listaEventos[e], i+1, j) and i+1 in Mes(self._mes).listaDiasLaborables():
+                        eventosPorDiaHora[i][j].append(e)
+        return eventosPorDiaHora
 
-        for i in range(31):
+    # retorna una arreglo con tres listas: [0]dias Laborales del evento, [1]dias laborables evento antes de cruce
+    #[2]dias laboralbes eventos despues de cruce
+    def listaDiasLaborablesEvento(self, evento):
+        diaInicio = 1 if (evento.fechaI < Mes(self._mes).primerDia()) else evento.fechaI.day
+        diaFin = Mes(self._mes).ultimoDia().day if (Mes(self._mes).ultimoDia() < evento.fechaF) else evento.fechaF.day
+        return [list(set([x for x in range(diaInicio, diaFin+1)]) & set(Mes(self._mes).listaDiasLaborables())),
+                list(set([] if evento.fechaICruce is None else [x for x in range(diaInicio, evento.fechaICruce.day)]) & set(Mes(self._mes).listaDiasLaborables())),
+                list(set([] if evento.fechaFCruce is None else [x for x in range(evento.fechaFCruce.day, diaFin)]) & set(Mes(self._mes).listaDiasLaborables()))]
+    
+    # retorna la capacidad en horas brutas y horas cruzadas de un evento y setea la fecha Inicial y Final de cruces del evento   
+    def horasProgramablesEvento(self, e):
+        horasCruzadasBrutas = horasCruzadasNetas = horasCruzadasNetasIniciales = horasCruzadasNetasFinales = 0
+        eventosPorDiaHora = self.eventosPorDiaHora()
+        diaMinCruce = diaMaxCruce = None
+        for i in range(Mes(self._mes).ultimoDia().day):
             for j in range(24):
-                print("[",i,",",j,"]...", programacionBruta[i][j])
+                if e in eventosPorDiaHora[i][j] and len(eventosPorDiaHora[i][j]) > 1:
+                    horasCruzadasBrutas += 1
+                    if diaMinCruce is None or date(2023, self._mes, i+1) < diaMinCruce: diaMinCruce = date(2023, self._mes, i+1)
+                    if diaMaxCruce is None or date(2023, self._mes, i+1) > diaMaxCruce: diaMaxCruce = date(2023, self._mes, i+1)
+        self._listaEventos[e].fechaICruce = diaMinCruce
+        self._listaEventos[e].fechaFCruce = diaMaxCruce
+        horasDelEvento = self._listaEventos[e].horaF - self._listaEventos[e].horaI
+        return { \
+            "ficha": (self._listaEventos[e].ficha), \
+            "horasBrutas" : horasDelEvento * len(self.listaDiasLaborablesEvento(self._listaEventos[e])[0]), \
+            "horasCruzadasBrutas" : horasCruzadasBrutas, \
+            "horasNoCruzadasNetasIniciales" : horasDelEvento * len(self.listaDiasLaborablesEvento(self._listaEventos[e])[1]), \
+            "horasNoCruzadasNetasFinales" : horasDelEvento * len(self.listaDiasLaborablesEvento(self._listaEventos[e])[2]) \
+        }
+    
+    # retorna un diccionario de las fichas con sus respectivos datos de horas brutas y horas cruzadas 
+    def horasProgramablesFicha(self):        
+        listaFichas = list(set([evento.ficha for evento in self._listaEventos]))
+        horasPorFicha = {}
+        for ficha in listaFichas:
+            horasPorFicha[ficha] = [0,0,0,0]
+        for e in range(len(self._listaEventos)):            
+            horasEvento = self.horasProgramablesEvento(e)
+            print(f"evento {e:2d}: horas brutas: {horasEvento['horasBrutas']:2d}, horas cruzadas brutas: {horasEvento['horasCruzadasBrutas']:2d}, horas no cruzadas netas iniciales : {horasEvento['horasNoCruzadasNetasIniciales']:2d}, horas no cruzadas netas finales : {horasEvento['horasNoCruzadasNetasFinales']:2d},")
+            horasPorFicha[horasEvento['ficha']][0] += horasEvento['horasBrutas']  
+            horasPorFicha[horasEvento['ficha']][1] += horasEvento['horasCruzadasBrutas']
+            horasPorFicha[horasEvento['ficha']][2] += horasEvento['horasNoCruzadasNetasIniciales']
+            horasPorFicha[horasEvento['ficha']][3] += horasEvento['horasNoCruzadasNetasFinales']
+        return horasPorFicha
 
-    def diasLaborablesEvento(self, fechaI, fechaF):
-        rangoDiasCorrientes = []
-        if (fechaI < datetime.date(2023, self.__mes, 1)):
-            rangoDiasCorrientes.append(1)
-        else:
-            rangoDiasCorrientes.append(fechaI.day)
-        if ((datetime.date(2023, self.__mes, 1) + relativedelta(day=31)) < fechaF):
-            rangoDiasCorrientes.append((datetime.date(2023, self.__mes, 1) + relativedelta(day=31)).day)
-        else:
-            rangoDiasCorrientes.append(fechaF.day)
-        diasCorrientesEvento = [x for x in range(rangoDiasCorrientes[0],rangoDiasCorrientes[1]+1)]
-        diasLaborablesEvento = list(set(diasCorrientesEvento) & set(DiasLaborables(self.__mes).diasLaborablesMes()))
-        print(diasLaborablesEvento)       
-        return diasLaborablesEvento
-    
-    def capacidadBrutaHoras(self):
-        capacidadBrutaHoras = 0
-        for evento in self.__listaEventos:
-            horasEvento = evento.getHoraF() - evento.getHoraI()
-            diasLaborablesEvento = self.diasLaborablesEvento(evento.getFechaI(), evento.getFechaF())
-            capacidadBrutaHoras += horasEvento * len(diasLaborablesEvento)      
-        if capacidadBrutaHoras < self.__horasAProgramar:
-            print("hay que crear eventos")
-        else:
-            print("en teoria los eventos actuales alcanzan pero hay que revisar que no se crucen")
-        return capacidadBrutaHoras
-    
     # Presenta en consola el resultado de la programacion    
     def resultado(self):
-#        capacidadBrutaHoras = self.capacidadBrutaHoras()
-        self.cruceDeEventos()
-       
-evento1 = Evento(123456, 6, 9, datetime.date(2023,3,1), datetime.date(2023,3,3))
-evento2 = Evento(123456, 8, 10, datetime.date(2023,3,12), datetime.date(2023,3,15))
-evento3 = Evento(123457, 8, 12, datetime.date(2023,3,11), datetime.date(2023,3,20))
-evento4 = Evento(123458, 11, 13, datetime.date(2023,3,1), datetime.date(2023,3,5))
-                
-#listaEventos = [evento1]
-listaEventos = [evento1, evento2, evento3]
-#listaEventos = [evento1, evento2, evento3, evento4]
-programador = Programador(listaEventos, 3, 80)
-programador.resultado()
+        eventosPorDiaHora = self.eventosPorDiaHora()    
+        for i in range(Mes(self._mes).ultimoDia().day):
+            for j in range(24):
+                if len(eventosPorDiaHora[i][j]) > 0:
+                    print(f"[dia: {i+1:2d}, horas: {j:2d} a {j+1:2d}]...Eventos:", eventosPorDiaHora[i][j])
+        print("****************")
+        print(self.horasProgramablesFicha())
+        for evento in listaEventos:
+            print(evento)
         
+# principal       
+evento0 = Evento(2675758, 6, 7, date(2023,4,1), date(2023,4,30))
+evento1 = Evento(2675759, 7, 8, date(2023,4,1), date(2023,4,30))
+evento2 = Evento(2626937, 12, 14, date(2023,4,1), date(2023,4,30))
+evento3 = Evento(2626938, 7, 9, date(2023,4,1), date(2023,4,30))
+evento4 = Evento(2626939, 9, 11, date(2023,4,1), date(2023,4,30))
+evento5 = Evento(2626940, 7, 8, date(2023,4,1), date(2023,4,30))
+evento6 = Evento(2675911, 12, 13, date(2023,4,1), date(2023,4,30))
+evento7 = Evento(2675912, 20, 22, date(2023,4,1), date(2023,4,30))
+evento8 = Evento(2675758, 13, 16, date(2023,4,1), date(2023,4,30))
+evento9 = Evento(2675759, 0, 0, date(2023,4,1), date(2023,4,30))
+evento10 = Evento(2626937, 22, 23, date(2023,4,1), date(2023,4,30))
+evento11 = Evento(2626938, 6, 7, date(2023,4,1), date(2023,4,30))
+evento12 = Evento(2626939, 8, 9, date(2023,4,1), date(2023,4,30))
+evento13 = Evento(2626940, 8, 10, date(2023,4,1), date(2023,4,30))
+evento14 = Evento(2675911, 6, 7, date(2023,4,1), date(2023,4,30))
+evento15 = Evento(2675912, 0, 0, date(2023,4,1), date(2023,4,30))
+
+#listaEventos = [evento0]
+#listaEventos = [evento0, evento1, evento2]
+#listaEventos = [evento0, evento1, evento2, evento3]
+listaEventos = [evento0, evento1, evento2, evento3, evento4, evento5, evento6, evento7, evento8, evento9, evento10, evento11, evento12, evento13, evento14, evento15]
+
+programador = Programador(listaEventos, 4, 160)
+programador.resultado()
---
+++ test1.py
@@ -0,0 +1,49 @@
+
+lista =[x for x in range(1,32)]
+for i in range(1,32):
+    print(i, " - ", lista[i-1], ";", end="")
+
+lista1 = [[j for j in range(24)] for i in range(1, 31)]
+print(lista1)
+
+class prueba:
+    def __init__(self):
+        self.__cedula
+        self.__nombre
+        self.__apellido
+
+    @property
+    def cedula(self):
+        return self.__cedula
+
+    @cedula.setter
+    def cedula(self, cedula):
+        self.__cedula = cedula
+        
+    @property 
+    def nombre(self): 
+        return self.__nombre 
+    
+    @nombre.setter 
+    def nombre(self, nombre): 
+        self.__nombre = nombre 
+
+    @property 
+    def apellido(self): 
+        return self.__apellido 
+
+    @apellido.setter 
+    def apellido(self, apellido): 
+        self.__apellido = apellido
+
+def decorador(f):
+    def funcion_nueva():
+        print("Funcionalidad extra")
+        f()
+    return funcion_nueva
+
+@decorador
+def funcion_inicial():
+    print("Funcionalidad inicial")
+
+funcion_inicial()
