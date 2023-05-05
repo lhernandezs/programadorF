3be6321b38f4ac4742d0d722db2ec56f7b4887d0
lhernandezs <lhernandezs331@gmail.com>
2023-05-05 14:08:38 -0500

    2C 5 may

--- rectangulos.py
+++ rectangulos.py
@@ -16,32 +16,32 @@ for j in range(len(listaY)):
          print(f"|{matrizHorasProgramadas[i][j]}", end = "")
     print("|")
 
-def encontrarRectangulo(xFin, yFin):
+def encontrarRectangulo():
     global xAnt
     global yAnt
     global pila
     if len(pila)> 0:
-        (xIni, yIni) = pila.pop()
+        (xIni, yIni, xFin, yFin) = pila.pop()
     else:
-        (xIni, yIni) = (10, 8)
+        (xIni, yIni, yFin, xFin) = (10, 8, 10, 8)
 
 
     for i in range(xIni, xFin):
         for j in range(yIni, yFin):
             if matrizHorasProgramadas[i][j] != " ":
                 matrizDeRectangulos.append(["Dentro", "R", xIni, j-1, yIni, yFin])
-                pila.append((i + 1, j))
-                pila.append((i, j + 1))
-                encontrarRectangulo(i-1, yFin)
+                pila.append((i + 1, j, xFin, yFin))
+                pila.append((i, j + 1, xFin, yFin))
+                encontrarRectangulo()
     else:
         matrizDeRectangulos.append(["Fuera", "T", xIni, xFin, yIni, yFin])
         if len(pila)>0:
-            (xIni,yIni) = pila.pop()
-            encontrarRectangulo(10, 8)
+            (xIni, yIni, xFin, yFin) = pila.pop()
+            encontrarRectangulo()
         else:
             return
                         
-pila = [(0,0)]
-encontrarRectangulo(10, 8)
+pila = [(0,0,10,8)]
+encontrarRectangulo()
 for x in range(len(matrizDeRectangulos)):
     print(f"sentido: {matrizDeRectangulos[x][0]}, marca: {matrizDeRectangulos[x][1]} -- horas: {matrizDeRectangulos[x][2]:2d} a {matrizDeRectangulos[x][3]:2d} -- dias: {matrizDeRectangulos[x][4]:2d} a {matrizDeRectangulos[x][5]:2d}")
