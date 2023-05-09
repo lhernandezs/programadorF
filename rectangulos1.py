073c62674cd6563af02cda2cedc70609295750cf
lhernandezs <lhernandezs331@gmail.com>
2023-05-09 17:09:56 -0500

    3C 9 may

--- rectangulos1.py
+++ rectangulos1.py
@@ -1,5 +1,5 @@
-ancho = 20
-alto = 14
+ancho = 10
+alto = 7
 
 listaX = [x for x in range(ancho)]
 listaY = [y for y in range(alto)]
@@ -10,16 +10,16 @@ matrizHorasProgramadas = [[" "  for y in listaY] for x in listaX]
 matrizHorasProgramadas[4][3] = "X"
 matrizHorasProgramadas[5][5] = "X"
 matrizHorasProgramadas[6][5] = "X"
-matrizHorasProgramadas[7][5] = "X"
-matrizHorasProgramadas[8][5] = "X"
-matrizHorasProgramadas[9][5] = "X"
-matrizHorasProgramadas[2][6] = "X"
-matrizHorasProgramadas[2][7] = "X"
-matrizHorasProgramadas[2][8] = "X"
-matrizHorasProgramadas[2][9] = "X"
-matrizHorasProgramadas[15][12] = "X"
-matrizHorasProgramadas[16][12] = "X"
-matrizHorasProgramadas[17][12] = "X"
+# matrizHorasProgramadas[7][5] = "X"
+# matrizHorasProgramadas[8][5] = "X"
+# matrizHorasProgramadas[9][5] = "X"
+# matrizHorasProgramadas[2][6] = "X"
+# matrizHorasProgramadas[2][7] = "X"
+# matrizHorasProgramadas[2][8] = "X"
+# matrizHorasProgramadas[2][9] = "X"
+# matrizHorasProgramadas[15][12] = "X"
+# matrizHorasProgramadas[16][12] = "X"
+# matrizHorasProgramadas[17][12] = "X"
 
 def encontrarRectangulo():
     ban = False
@@ -58,72 +58,73 @@ print("*********************************************")
 print("************* c o m e n z o *****************")
 print("*********************************************")
 
-matrizDeRectangulosNew = matrizDeRectangulos
+relleno = str(chr(35))  
+arregloMatrizRectangulos = False
+if arregloMatrizRectangulos:
+    matrizDeRectangulosNew = []
+    while len(matrizDeRectangulos)>0:
+        (indicador,xIni, yIni, xFin, yFin) = matrizDeRectangulos.pop()
+        xEIni, yEIni, xEFin, yEFin = xIni, yIni, xFin, yFin
+        ban = False
+        for rn in range(len(matrizDeRectangulosNew)):
+            if len(matrizDeRectangulosNew)>0:
+                (indicador, xnIni, ynIni, xnFin, ynFin) = matrizDeRectangulosNew[rn]
+                if   xIni != xnIni and xFin == xnFin and yIni == ynIni and yFin == ynFin:
+                    xEIni = xIni if xIni <= xnIni else xnIni
+                    ban = True
+                elif xIni == xnIni and xFin != xnFin and yIni == ynIni and yFin == ynFin:
+                    xEFin = xFin if xFin >= xnFin else xnFin
+                    ban = True
+                elif xIni == xnIni and xFin == xnFin and yIni != ynIni and yFin == ynFin:
+                    yEIni = yIni if yIni <= ynIni else ynIni
+                    ban = True
+                elif xIni == xnIni and xFin == xnFin and yIni == ynIni and yFin != ynFin:
+                    yEFin = yFin if yFin >= ynFin else ynFin
+                    ban = True        
+        if not ban:
+            matrizDeRectangulosNew.append([indicador, xEIni, yEIni, xEFin, yEFin])
+else:
+    matrizDeRectangulosNew = matrizDeRectangulos
 
-# matrizDeRectangulosNew = []
-# while len(matrizDeRectangulos)>0:
-#     (indicador,xIni, yIni, xFin, yFin) = matrizDeRectangulos.pop()
-#     xEIni, yEIni, xEFin, yEFin = xIni, yIni, xFin, yFin
-#     ban = False
-#     for rn in range(len(matrizDeRectangulosNew)):
-#         if len(matrizDeRectangulosNew)>0:
-#             (indicador, xnIni, ynIni, xnFin, ynFin) = matrizDeRectangulosNew[rn]
-#             if   xIni != xnIni and xFin == xnFin and yIni == ynIni and yFin == ynFin:
-#                 xEIni = xIni if xIni <= xnIni else xnIni
-#                 ban = True
-#             elif xIni == xnIni and xFin != xnFin and yIni == ynIni and yFin == ynFin:
-#                 xEFin = xFin if xFin >= xnFin else xnFin
-#                 ban = True
-#             elif xIni == xnIni and xFin == xnFin and yIni != ynIni and yFin == ynFin:
-#                 yEIni = yIni if yIni <= ynIni else ynIni
-#                 ban = True
-#             elif xIni == xnIni and xFin == xnFin and yIni == ynIni and yFin != ynFin:
-#                 yEFin = yFin if yFin >= ynFin else ynFin
-#                 ban = True        
-#     if not ban:
-#         matrizDeRectangulosNew.append([indicador, xEIni, yEIni, xEFin, yEFin])
 
-
-# for x in range(len(matrizDeRectangulosNew)):
-#     print(f"Lugar: {matrizDeRectangulosNew[x][0]} -- coordenada Inicial: ({matrizDeRectangulosNew[x][1]:2d} , {matrizDeRectangulosNew[x][2]:2d}) -- coordenada Final: ({matrizDeRectangulosNew[x][3]:2d}, {matrizDeRectangulosNew[x][4]:2d})")
-#     print("   ", end="")
-#     for a in range(matrizDeRectangulosNew[x][1], matrizDeRectangulosNew[x][3] +1 ):
-#         for b in range(matrizDeRectangulosNew[x][2], matrizDeRectangulosNew[x][4] +1 ):
-#             matrizHorasProgramadas[a][b] = "O" 
-
-#     for i in range(len(listaX)):
-#         print(f"|{i:2d}", end = "")
-#     print("|")
-#     for j in range(len(listaY)):
-#         print(f" {j:2d}", end = "")
-#         for i in range(len(listaX)):
-#             print(f"|{(matrizHorasProgramadas[i][j]).rjust(2)}", end = "")
-#         print("|")
-
-#     for a in range(matrizDeRectangulosNew[x][1], matrizDeRectangulosNew[x][3] +1 ):
-#         for b in range(matrizDeRectangulosNew[x][2], matrizDeRectangulosNew[x][4] +1 ):
-#             matrizHorasProgramadas[a][b] = " " 
-
-with open("text.txt","w") as file:
-
+terminal = True
+if terminal:
     for x in range(len(matrizDeRectangulosNew)):
-        file.write(f"Lugar: {matrizDeRectangulosNew[x][0]} -- coordenada Inicial: ({matrizDeRectangulosNew[x][1]:2d} , {matrizDeRectangulosNew[x][2]:2d}) -- coordenada Final: ({matrizDeRectangulosNew[x][3]:2d}, {matrizDeRectangulosNew[x][4]:2d})\n")
-        file.write("   ")
+        print(f"Lugar: {matrizDeRectangulosNew[x][0]} -- coordenada Inicial: ({matrizDeRectangulosNew[x][1]:2d} , {matrizDeRectangulosNew[x][2]:2d}) -- coordenada Final: ({matrizDeRectangulosNew[x][3]:2d}, {matrizDeRectangulosNew[x][4]:2d})")
+        print("   ", end="")
         for a in range(matrizDeRectangulosNew[x][1], matrizDeRectangulosNew[x][3] +1 ):
             for b in range(matrizDeRectangulosNew[x][2], matrizDeRectangulosNew[x][4] +1 ):
-                matrizHorasProgramadas[a][b] = "O" 
-
+                matrizHorasProgramadas[a][b] = relleno 
         for i in range(len(listaX)):
-            file.write(f"|{i:2d}")
-        file.write("|\n")
+            print(f"|{i:2d}", end = "")
+        print("|")
         for j in range(len(listaY)):
-            file.write(f" {j:2d}")
+            print(f" {j:2d}", end = "")
             for i in range(len(listaX)):
-                file.write(f"|{(matrizHorasProgramadas[i][j]).rjust(2)}")
-            file.write("|\n")
-
+                print(f"|{(matrizHorasProgramadas[i][j]).rjust(2)}", end = "")
+            print("|")
         for a in range(matrizDeRectangulosNew[x][1], matrizDeRectangulosNew[x][3] +1 ):
             for b in range(matrizDeRectangulosNew[x][2], matrizDeRectangulosNew[x][4] +1 ):
                 matrizHorasProgramadas[a][b] = " " 
+else:
+    with open("text.txt","w") as file:
+        for x in range(len(matrizDeRectangulosNew)):
+            file.write(f"Lugar: {matrizDeRectangulosNew[x][0]} -- coordenada Inicial: ({matrizDeRectangulosNew[x][1]:2d} , {matrizDeRectangulosNew[x][2]:2d}) -- coordenada Final: ({matrizDeRectangulosNew[x][3]:2d}, {matrizDeRectangulosNew[x][4]:2d})\n")
+            file.write("   ")
+            for a in range(matrizDeRectangulosNew[x][1], matrizDeRectangulosNew[x][3] +1 ):
+                for b in range(matrizDeRectangulosNew[x][2], matrizDeRectangulosNew[x][4] +1 ):
+                    matrizHorasProgramadas[a][b] = relleno 
 
-file.close()
+            for i in range(len(listaX)):
+                file.write(f"|{i:2d}")
+            file.write("|\n")
+            for j in range(len(listaY)):
+                file.write(f" {j:2d}")
+                for i in range(len(listaX)):
+                    file.write(f"|{(matrizHorasProgramadas[i][j]).rjust(2)}")
+                file.write("|\n")
+
+            for a in range(matrizDeRectangulosNew[x][1], matrizDeRectangulosNew[x][3] +1 ):
+                for b in range(matrizDeRectangulosNew[x][2], matrizDeRectangulosNew[x][4] +1 ):
+                    matrizHorasProgramadas[a][b] = " " 
+    file.close()
